90a14c313da75d578312508fd78c08c1b6b6dc8c
TRatnakumari <143867699+TRatnakumari@users.noreply.github.com>
2023-09-08 12:35:11 +0530

    Add files via upload

---
+++ CmdLineArgsV3.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class CmdLineArgsV3 extends Object {
+    public static void main(String args[]) {
+        CmdLineArgsV3 obj=new CmdLineArgsV3();
+        if(args.length!=2)
+        {
+            System.out.println("Invalid number of arguments \n You need to pass two integers less than 10000");
+        }
+        else
+        {
+           int x = Integer.parseInt(args[0]);
+           int y = Integer.parseInt(args[1]);
+           int z = x+y;
+           System.out.println("The classname is : "+obj.getClass());
+           System.out.println("Sum of " +x+ " and " +y+ " is " +z);
+        }
+}
+}
